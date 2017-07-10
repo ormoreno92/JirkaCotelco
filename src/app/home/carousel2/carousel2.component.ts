ff7efb3efbd914f9724578f3329ff2321b987d72
Omar Moreno <ormoreno@unipanamericana.edu.co>
2017-07-10 18:13:25 -0500

    changes on home
    some changes to add structure to the page.

--- src/app/home/carousel2/carousel2.component.ts
+++ src/app/home/carousel2/carousel2.component.ts
@@ -1,3 +1,5 @@
+declare var jQuery: any;
+import { DataServiceService } from '../../data-service.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel2.component.less']
 })
 export class Carousel2Component implements OnInit {
-
-  constructor() { }
-
+  images;
+  constructor(private dataService: DataServiceService) { }
   ngOnInit() {
+    this.dataService.getHomeBannerImages()
+      .subscribe(dataH => this.images = dataH, error => console.log(error));
   }
-
+  private drawBannerImages(data: any): void {
+    console.log(data);
+  }
 }
