22d90016429d23982053d4b3931ef90f487fdace
Omar Moreno <ormoreno@unipanamericana.edu.co>
2017-09-17 23:11:31 -0500

    map implemented

--- src/app/chapters/chapters.component.ts
+++ src/app/chapters/chapters.component.ts
@@ -1,4 +1,8 @@
+import { DataServiceService } from '../data-service.service';
 import { Component, OnInit } from '@angular/core';
+
+declare var Jquery: any;
+declare var $: any;
 
 @Component({
   selector: 'app-chapters',
@@ -6,10 +10,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chapters.component.less']
 })
 export class ChaptersComponent implements OnInit {
-
-  constructor() { }
+  chapters = [];
+  chapter;
+  constructor(private dataService: DataServiceService) { }
 
   ngOnInit() {
+    this.dataService.getChapters()
+      .subscribe(dataH => this.drawInfo(dataH), error => console.log(error));
   }
 
+  private drawInfo(dataH: any): void {
+    console.log(dataH);
+    this.chapters = dataH;
+    this.chapter = dataH[0];
+    this.drawMap();
+  }
+  private drawMap(): void {
+    $('#map-colombia').CSSMap({
+      'size': 960,
+      'mapStyle': 'blue',
+      'cities': true,
+      'tooltips': false,
+      'responsive': 'auto',
+      'tapOnce': true,
+      'pins': {
+        'enable': true,
+        'pinsId': '#demo-markers',
+        'mapSize': 960,
+        'markerPosition': 'middle',
+        'tooltipPosition': 'top',
+        'tooltipOnClick': false,
+        'clickableRegions': true
+      }
+    });
+  }
 }
